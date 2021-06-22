d600d5e01f1036cd6ac759ff5e8a3ef50199c072
Melinda <melinda.sava.td@gmail.com>
2021-06-22 15:33:55 +0200

    first Vue3 CLI proj, props

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>{{ title }}</h1>
+<input type="text" ref="name">
+<button @click="handleClick">click me</button>
+<div v-if="showModal">
+  <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+</div>
+  <p>welcome</p>
+<button @click="toggleModal">open modal </button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+  data() {
+    return {
+      title: "My First Vue App :)",
+      header: "Sign-up for the giveaway!",
+      text: "Grab your new sthg for half price now.",
+      showModal: false
+    }
+  },
+  methods: {
+    handleClick() {
+      // console.log(this.$refs.name.value);
+      // this.$refs.name.classList.toggle('active');
+      this.$refs.name.focus()
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    }
   }
 }
 </script>
@@ -22,5 +46,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #ddd;
+  display:inline-block;
+  padding-bottom: 10px;
+}
+.active{
+  background:honeydew;
+  color:indianred;
 }
 </style>
